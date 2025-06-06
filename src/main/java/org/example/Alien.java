072937e974c7b50c74561b5af1222c8e7ab6e579
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-06-06 21:29:32 +0530

    getter setter difficulty

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -12,4 +12,19 @@ public class Alien {
         System.out.println("Coding..");
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
 }
