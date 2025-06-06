b3cbc7a32057ffb3cd8e40b11d2eee43528764d4
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-06-06 12:33:11 +0530

    laptop class created

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Alien {
 
+    public Alien(){
+        System.out.println("Object Created");
+    }
+
     public void code(){
         System.out.println("Coding..");
     }
---
+++ src/main/java/org/example/Laptop.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Laptop {
+
+    public Laptop(){
+        System.out.println("Laptop object created");
+    }
+
+}
