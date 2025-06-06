54e2bfcd328a135afc9a72df92a2fde07372e8c2
Suhail Akhtar <suhailakhtar039@gmail.com>
2025-06-06 12:00:41 +0530

    bean factory initialized

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,7 +11,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        Alien obj = new Alien();
+        ApplicationContext context = new ClassPathXmlApplicationContext();
+        Alien obj = (Alien) context.getBean("alien");
         obj.code();
 
     }
